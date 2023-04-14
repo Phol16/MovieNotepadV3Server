aff58b7137cd8d118e5be9d33476062279c6cff9
Phol <Pholibertlim@gmail.com>
2023-04-15 01:40:21 +0800

    another prob about cookies wew

--- src/controllers/users/authentication.ts
+++ src/controllers/users/authentication.ts
@@ -86,7 +86,7 @@ export const logIn = async (req: Request, res: Response) => {
     await user.save();
 
     //store sessiontoken in the cookie with no expiry date becomes a session cookie
-    res.cookie('MN_sessionToken', user.authentication.sessionToken);
+    res.cookie('MN_sessionToken', user.authentication.sessionToken, {domain:'https://movienotepadclient.onrender.com',sameSite:'none'});
     return res.status(200).json({
       data: user,
       message: 'success',
